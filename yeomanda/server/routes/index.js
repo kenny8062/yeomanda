863df7118bca04f823f6a94b0ba6f93008cde24c
jaemin <jjmmll0727@gmail.com>
2021-07-13 16:13:17 +0900

    todo: controller

--- yeomanda/server/routes/index.js
+++ yeomanda/server/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const verifyToken = require('../middlewares/verifyToken');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/user', require('./user'));
+
 
 module.exports = router;
