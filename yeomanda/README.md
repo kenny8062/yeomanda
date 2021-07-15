c88805e2f77b13550f647bb5ddd0d3f71435ecae
jaemin <jjmmll0727@gmail.com>
2021-07-15 11:19:14 +0900

    setting for aws dyanmodb

---
+++ yeomanda/README.md
@@ -0,0 +1,5 @@
+### STACK
+
+##### database : aws dynamodb 
+##### image store : s3 
+- s3 cannot store file bigger than 400KB
---
+++ yeomanda/server/controller/userController.js
@@ -0,0 +1,7 @@
+const util = require('../modules/util');
+const responseMessage = require('../modules/responseMessage');
+const statusCode = require('../modules/statusCode');
+const bcrypt = require('bcryptjs');
+const jwt = require('../modules/jwt')
+
+
