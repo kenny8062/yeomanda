5df3f810ae2c87d92e97c38d7e121d7df1a5f52e
jaemin <jjmmll0727@gmail.com>
2021-08-02 16:58:15 +0900

    travelController(search & insert new plan) 90% done, todo: refactoring

--- yeomanda/server/controller/travelersController.js
+++ yeomanda/server/controller/travelersController.js
@@ -5,7 +5,16 @@ const statusCode = require('../modules/statusCode');
 /**
  * connection to aws mysql server
  */
-const mysql_config = require('../config/aws/Travelers')
+
+    
+    /**
+     * show travelers who is located closely with logged in user.
+     * logic for find who is closed,
+     * but now, just show all travlers who stored in traveler database.
+     */
+
+const mysql_config = require('../config/aws/Travelers');
+const { json } = require('express');
 const conn = mysql_config.init()
 mysql_config.connect(conn)
 
@@ -23,19 +32,26 @@ const showTravelers = async(req, res) => {
 
 }
 const registerPlan = async(req, res) => {
-    const { location_gps, travelMate, travleDate } = req.body
-    
-    
-    /**
-     * show travelers who is located closely with logged in user.
-     * logic for find who is closed,
-     * but now, just show all travlers who stored in traveler database.
-     */
-
-    
-
-    
+    const plan = req.body
+    const jsonPlan = JSON.parse(JSON.stringify(plan));
+    for(var i=0; i<Object.keys(jsonPlan).length; i++){
+        const params = {
+            email : jsonPlan[i]["travelMate"].toString(),
+            location_gps : jsonPlan[i]["location_gps"].toString(),
+            team_no : 0,
+            travelDate : jsonPlan[i]["travelDate"].toString(),
+            isfinished : 0,
+        }
+        const sql = 'insert into travel_with set ?;'
+        conn.query(sql, params, function(err, data){
+            if(err){
+                return res.send(`failed to store data to database at ${i+1} try`)
+            }
+        })
+    }
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.QUERY_SUCCESS, "success to store new plan!!"))
 }
+
 
 
 module.exports = {
