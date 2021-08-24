3df697cd948dc00a399ceb84fc58ee0afe4e313c
jaemin <jjmmll0727@gmail.com>
2021-08-24 15:37:32 +0900

    update for showTravelers response

--- yeomanda/server/controller/travelersController.js
+++ yeomanda/server/controller/travelersController.js
@@ -28,6 +28,24 @@ const getLocation = async(latlng) => {
     };
 }
 
+/**
+ * grouping function
+ */
+function groupBy(list, keyGetter) {
+    const map = new Map();
+    list.forEach((item) => {
+         const key = keyGetter(item);
+         const collection = map.get(key);
+         if (!collection) {
+             map.set(key, [item]);
+         } else {
+             collection.push(item);
+         }
+    });
+    return map;
+}
+
+
 // right after login api
 const showTravelers = async(req, res) => {
     const { latitude, longitude } = req.body
@@ -38,7 +56,6 @@ const showTravelers = async(req, res) => {
     const locationResult = await getLocation(latlng)
     const country_code = locationResult.data.plus_code.global_code // country code ex)87G8M376+PJ - koera
     const country = country_code.slice(0,4);
-    console.log(country)
     /**
      * travel_with table에서 찾는 데이터들에서 이메일을 추출하여, dynamodb user table 에서 pk로 접근하여 name을 뽑아낸다.
      * req에서 받는 데이터를 가지고 현재 사용자의 위치를 찾는데, global code 의 앞 4자리(국가)만을 이용하여 같은 나라에 있는 사용자의 회원정보들을 response 한다.
@@ -63,20 +80,45 @@ const showTravelers = async(req, res) => {
             "region_info"
              */
 
-            // email을 가지고 USER table에서 name을 가져온다.
-            const nameList = []
-            for(var i=0; i<teams.length; i++){
-                const params = {
-                    TableName : userConfig.aws_table_name,
-                    KeyConditionExpression: 'email = :i',
-                    ExpressionAttributeValues: {
-                        ':i' : teams[i].email
-                    }
-                };
-                const result = await docClient.query(params).promise()
-                nameList.push(result.Items[0].name)
+            var travelers = [] // 여행객들의 정보를 담기 위한 배열
+            var teamList = []
+            teams.forEach((e) => {
+                teamList.push(e.team_no)
+            })
+            const teamListNoOverlap = Array.from(new Set(teamList)); // 중복없는 team_no list
+            const grouped = groupBy(teams, team => team.team_no); // 원하는 key값으로 매핑할 수 있다.
+            
+            for(var i=0; i<teamListNoOverlap.length; i++){
+                var travelersEmail = [] // 근처에 있는 여행객들의 이메일 정보를 담을 배열
+                for(var j=0; j<grouped.get(teamListNoOverlap[i]).length; j++){
+                    travelersEmail.push(grouped.get(teamListNoOverlap[i])[j].email)
+                }
+
+                const nameList = []
+                for(var m=0; m<grouped.get(teamListNoOverlap[i]).length; m++){
+                    const params = {
+                        TableName : userConfig.aws_table_name,
+                        KeyConditionExpression: 'email = :m',
+                        ExpressionAttributeValues: {
+                            ':m' : grouped.get(teamListNoOverlap[i])[m].email
+                        }
+                    };
+                    console.log(grouped.get(teamListNoOverlap[i]))
+                    const result = await docClient.query(params).promise()
+                    nameList.push(result.Items[0].name)
+                }
+                
+                travelers.push({
+                    "email" : travelersEmail, 
+                    "team_no" : teamListNoOverlap[i], 
+                    "location_gps" : grouped.get(teamListNoOverlap[i])[0].location_gps,
+                    "travelDate" : grouped.get(teamListNoOverlap[i])[0].travelDate,
+                    "isfinished" : grouped.get(teamListNoOverlap[i])[0].isfinished,
+                    "region_info" : grouped.get(teamListNoOverlap[i])[0].region_info,
+                    "name" : nameList
+                })
             }
-            return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.QUERY_SUCCESS, { teams, nameList }))
+            return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.QUERY_SUCCESS, travelers))
         }
     })
 }
