ca056f60243a0bda2231476ff1996ab965888124
jjmmll0727 <jjmmll0727@gmail.com>
2021-09-06 00:52:41 +0900

    chimchak1

--- yeomanda/server/routes/index.js
+++ yeomanda/server/routes/index.js
@@ -7,7 +7,7 @@ router.use('/travelers', require('./travelers'));
 router.use('/markup', require('./markup'));
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'chimchak5' });
+    res.render('index', { title: 'chimchak1' });
 });
 
 
