6ed697acd86db3cb33288d50f04ba387f9b18b40
jjmmll0727 <jjmmll0727@gmail.com>
2021-09-12 23:49:24 +0900

    what's wrong 3

--- yeomanda/server/routes/index.js
+++ yeomanda/server/routes/index.js
@@ -9,7 +9,7 @@ router.use('/menuBar', verifyToken.checkToken, require('./menuBar'));
 
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: ' yeomanda please !!! ' });
+    res.render('index', { title: ' what;s wrong !!! ' });
 })
 
 
